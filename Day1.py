470db3dae643d6a75b06ef9734ceb4b7cdd60116
vnag80 <VNAG80@gmail.com>
2022-12-01 17:40:04 -0500

    Day1 Completed

---
+++ Day1.py
@@ -0,0 +1,6 @@
+name = input("Please enter your Name \n");
+print("Hello " + name);
+city = input("Please enter your city \n");
+pet = input("Please enter your pet name \n");
+print(" Your band name could be :" +  city + " " + pet);
+
