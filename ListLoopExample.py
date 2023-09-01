37521dfb36070a829a905903c9510c88f05782b7
vnag80 <VNAG80@gmail.com>
2023-09-01 15:41:29 -0400

    test

--- ListLoopExample.py
+++ ListLoopExample.py
@@ -1,4 +1,18 @@
+
+
 fruits = ["Apple","Grape","kiwi"]
-for fruit in fruits:
-    print(fruit);
-    
+for index,fruit in enumerate(fruits):
+    print(index)
+    print(fruit)
+
+nums = [1,1,1,2,2,2]
+expectedNums = []
+length = len(nums)
+prev = 0
+for i in range(0, length - 1):
+
+    if nums[i] != nums[i + 1]:
+        nums.remove()
+        expectedNums.append(nums[i])
+expectedNums.append(nums[length - 1])
+print(expectedNums)
